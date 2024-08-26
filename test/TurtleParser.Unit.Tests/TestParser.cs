6e08f912c3740e5637ce18cd8d2dcb59225f6af8
Dag Hovland <dhovl@equinor.com>
2024-08-27 00:07:26 +0200

    Full turtle 1.2 antlr file

--- test/TurtleParser.Unit.Tests/TestParser.cs
+++ test/TurtleParser.Unit.Tests/TestParser.cs
@@ -8,6 +8,14 @@ public class TestParser
         => TurtleAntlr.Parser.ParseString(ontology);
 
     [Fact]
+    public void TestSingleTriple()
+    {
+        var ont = TestOntology("<http://example.org/subject> <http://example.org/predicate> <http://example.org/object> .");
+        Assert.NotNull(ont);
+    }
+    
+    
+    [Fact]
     public void Test1()
     {
         var ontology = File.ReadAllText("example1.ttl");
