d8b9c56aa2d468a3ca7bf24ffaf299d48d7d1cf6
Dag Hovland <dhovl@equinor.com>
2024-08-27 23:52:37 +0200

    More debugging in testing

--- test/TurtleParser.Unit.Tests/TestParser.cs
+++ test/TurtleParser.Unit.Tests/TestParser.cs
@@ -5,7 +5,17 @@ namespace TurtleParser.Unit.Tests;
 public class TestParser
 {
     public ALC.OntologyDocument TestOntology(string ontology)
-        => TurtleAntlr.Parser.ParseString(ontology);
+    {
+        try
+        {
+            return TurtleAntlr.Parser.ParseString(ontology);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return null;
+        }
+    }
 
     [Fact]
     public void TestSingleTriple()
@@ -18,7 +28,11 @@ public class TestParser
     public void TestSpecExample21()
     {
         var ont = TestOntology(
-            "<http://example.org/#spiderman>\n  <http://www.perceive.net/schemas/relationship/enemyOf>\n    <http://example.org/#green-goblin> .");
+            """
+            <http://example.org/#spiderman>
+            <http://www.perceive.net/schemas/relationship/enemyOf>
+            <http://example.org/#green-goblin> .
+            """);
         Assert.NotNull(ont);
     }
     
@@ -27,7 +41,10 @@ public class TestParser
     public void TestSpecExamplePredicateList()
     {
         var ont = TestOntology(
-            "<http://example.org/#spiderman> <http://www.perceive.net/schemas/relationship/enemyOf> <http://example.org/#green-goblin> ;\n    <http://xmlns.com/foaf/0.1/name> \"Spiderman\" .\n    ");
+            """
+            <http://example.org/#spiderman> <http://www.perceive.net/schemas/relationship/enemyOf> <http://example.org/#green-goblin> ;
+                <http://xmlns.com/foaf/0.1/name> "Spiderman" .
+            """);
         Assert.NotNull(ont);
     }
     
@@ -35,7 +52,32 @@ public class TestParser
     public void TestAllIriWritings()
     {
         var ont = TestOntology(
-            "\n# A triple with all resolved IRIs\n<http://one.example/subject1> <http://one.example/predicate1> <http://one.example/object1> .\n\n@base <http://one.example/> .\n<subject2> <predicate2> <object2> .     # relative IRI references, e.g., http://one.example/subject2\n\nBASE <http://one.example/>\n<subject2> <predicate2> <object2> .     # relative IRI references, e.g., http://one.example/subject2\n\n@prefix p: <http://two.example/> .\np:subject3 p:predicate3 p:object3 .     # prefixed name, e.g., http://two.example/subject3\n\nPREFIX p: <http://two.example/>\np:subject3 p:predicate3 p:object3 .     # prefixed name, e.g., http://two.example/subject3\n\n@prefix p: <path/> .                    # prefix p: now stands for http://one.example/path/\np:subject4 p:predicate4 p:object4 .     # prefixed name, e.g., http://one.example/path/subject4\n\nPrEfIx : <http://another.example/>       # empty prefix\n:subject5 :predicate5 :object5 .        # prefixed name, e.g., http://another.example/subject5\n\n:subject6 a :subject7 .                 # same as :subject6 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> :subject7 .\n\n<http://伝言.example/?user=أ&channel=R%26D> a :subject8 . # a multi-script subject IRI .");
+            """
+            # A triple with all resolved IRIs
+            <http://one.example/subject1> <http://one.example/predicate1> <http://one.example/object1> .
+
+            @base <http://one.example/> .
+            <subject2> <predicate2> <object2> .     # relative IRI references, e.g., http://one.example/subject2
+
+            BASE <http://one.example/>
+            <subject2> <predicate2> <object2> .     # relative IRI references, e.g., http://one.example/subject2
+
+            @prefix p: <http://two.example/> .
+            p:subject3 p:predicate3 p:object3 .     # prefixed name, e.g., http://two.example/subject3
+
+            PREFIX p: <http://two.example/>
+            p:subject3 p:predicate3 p:object3 .     # prefixed name, e.g., http://two.example/subject3
+
+            @prefix p: <path/> .                    # prefix p: now stands for http://one.example/path/
+            p:subject4 p:predicate4 p:object4 .     # prefixed name, e.g., http://one.example/path/subject4
+
+            PrEfIx : <http://another.example/>       # empty prefix
+            :subject5 :predicate5 :object5 .        # prefixed name, e.g., http://another.example/subject5
+
+            :subject6 a :subject7 .                 # same as :subject6 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> :subject7 .
+
+            <http://伝言.example/?user=أ&channel=R%26D> a :subject8 . # a multi-script subject IRI .
+            """);
         Assert.NotNull(ont);
     }
     
@@ -44,7 +86,13 @@ public class TestParser
     public void TestNumberLiterals()
     {
         var ont = TestOntology(
-            "\nPREFIX : <http://example.org/elements/>\n<http://en.wikipedia.org/wiki/Helium>\n    :atomicNumber 2 ;               # xsd:integer\n    :atomicMass 4.002602 ;          # xsd:decimal\n    :specificGravity 1.663E-4 .     # xsd:double");
+            """
+                PREFIX : <http://example.org/elements/>
+                <http://en.wikipedia.org/wiki/Helium>
+                    :atomicNumber 2 ;               # xsd:integer
+                    :atomicMass 4.002602 ;          # xsd:decimal
+                    :specificGravity 1.663E-4 .     # xsd:double
+            """);
         Assert.NotNull(ont);
     }
     
