d9b350204fc649d906e8ba8a7a05f7523b15858b
Dag Hovland <dhovl@equinor.com>
2024-09-25 18:37:36 +0200

    Brushed up test files

--- test/NugetTest/TestApi.cs
+++ test/NugetTest/TestApi.cs
@@ -19,35 +19,35 @@ public class TestApi(ITestOutputHelper output)
         Assert.NotNull(ont);
         var enemies = ont.GetTriplesWithPredicate(new IriReference("http://www.perceive.net/schemas/relationship/enemyOf"));
         enemies.Count().Should().Be(2, "There are two trples with enemies");
-        
+
 
 
     }
-    
-    
+
+
     [Fact]
     public void TestDbPedia()
     {
-        var ontology = new FileInfo("TestData/test2.ttl");
+        var ontology = new FileInfo("DbpediaTests/test2.ttl");
         var ont = AlcTableau.Api.TurtleParser.Parse(ontology, outputWriter);
 
         Assert.NotNull(ont);
         var subjects = ont.GetTriplesWithPredicate(new IriReference("http://purl.org/dc/terms/subject"));
         subjects.Count().Should().BeGreaterThan(200, "There are many triples with subjects");
     }
-    
-    
-    
+
+
+
     [Fact]
     public void TestDbPediaOntology()
     {
-        var ontology = new FileInfo("TestData/ontology--DEV_type=parsed_sorted.nt");
+        var ontology = new FileInfo("DbpediaTests/test1.ttl");
         var ont = AlcTableau.Api.TurtleParser.Parse(ontology, outputWriter);
 
         Assert.NotNull(ont);
         var labels = ont.GetTriplesWithSubjectPredicate(
-            new IriReference("http://dbpedia.org/datatype/FuelEfficiency"), 
+            new IriReference("http://dbpedia.org/ontology/NaturalEvent"),
             new IriReference("http://www.w3.org/2000/01/rdf-schema#label"));
-        labels.Count().Should().Be(2, "There are two labels on fuel efficiency");
+        labels.Count().Should().Be(7, "There are two labels on natural event");
     }
 }
