d51d765addf33c6efd0427f3e2186d417aca36ae
Dag Hovland <dhovl@equinor.com>
2025-01-31 12:58:03 +0100

    Added negative assertion tests for owl 2 alc

--- test/OWL2ALC.Tests/Tests.fs
+++ test/OWL2ALC.Tests/Tests.fs
@@ -8,6 +8,7 @@
 namespace DagSemTools.OWL2ALC
 open DagSemTools.AlcTableau
 open DagSemTools.AlcTableau.ALC
+open DagSemTools.AlcTableau.DataRange
 open DagSemTools.Ingress
 open DagSemTools.OwlOntology
 open IriTools
@@ -39,6 +40,29 @@ module Tests =
       
 
     [<Fact>]
+    let ``Single className is translated`` () =
+        let subclassIri = (IriReference "https://example.com/subclass")
+        let subClass = (ClassName (FullIri subclassIri))
+        let translatedClass = Translator.translateClass logger subClass
+        translatedClass.Should().Be(ConceptName subclassIri)
+    
+    [<Fact>]
+    let ``Single axiom Owl ontology is translated`` () =
+        let subclassIri = (IriReference "https://example.com/subclass")
+        let subClass = (ClassName (FullIri subclassIri))
+        let superclassIri = (IriReference "https://example.com/superclass")
+        let superClass = (ClassName (FullIri superclassIri))
+        let axiom = AxiomClassAxiom (SubClassOf ([],subClass, superClass)) 
+        let emptyOntology = new DagSemTools.OwlOntology.Ontology([],
+                                                                 DagSemTools.OwlOntology.ontologyVersion.UnNamedOntology,
+                                                                 [],
+                                                                 [axiom])
+        let translatedOntology = Translator.translate logger emptyOntology
+        let expectedAxiom = Inclusion (ConceptName subclassIri, ConceptName superclassIri) 
+        let expectedOntology = OntologyDocument.Ontology ([], ontologyVersion.UnNamedOntology, ([expectedAxiom],[]))
+        translatedOntology.Should().Be(expectedOntology)
+    
+    [<Fact>]
     let ``Simple class union is translated`` () =
         let subclassIri = (IriReference "https://example.com/subclass")
         let class1 = (ClassName (FullIri subclassIri))
@@ -47,6 +71,52 @@ module Tests =
         let union = ObjectUnionOf [class1; class2]
         let translatedClass = Translator.translateClass logger union
         translatedClass.Should().Be(Disjunction (ConceptName subclassIri, ConceptName superclassIri))
+    
+    [<Fact>]
+    let ``Triple class union is translated`` () =
+        let subclassIri = (IriReference "https://example.com/subclass")
+        let class1 = (ClassName (FullIri subclassIri))
+        let superclassIri = (IriReference "https://example.com/superclass")
+        let class2 = (ClassName (FullIri superclassIri))
+        let classIri3 = (IriReference "https://example.com/class3")
+        let class3 = (ClassName (FullIri classIri3))
+        let union = ObjectUnionOf [class1; class2; class3]
+        let translatedClass = Translator.translateClass logger union
+        translatedClass.Should().Be(Disjunction (ConceptName subclassIri, Disjunction (ConceptName superclassIri, ConceptName classIri3)))
+    
+    [<Fact>]
+    let ``Simple class intersection is translated`` () =
+        let subclassIri = (IriReference "https://example.com/subclass")
+        let class1 = (ClassName (FullIri subclassIri))
+        let superclassIri = (IriReference "https://example.com/superclass")
+        let class2 = (ClassName (FullIri superclassIri))
+        let union = ObjectIntersectionOf [class1; class2]
+        let translatedClass = Translator.translateClass logger union
+        translatedClass.Should().Be(Conjunction (ConceptName subclassIri, ConceptName superclassIri))
+    
+    
+    
+    
+    [<Fact>]
+    let ``Triple class intersection is translated`` () =
+        let subclassIri = (IriReference "https://example.com/subclass")
+        let class1 = (ClassName (FullIri subclassIri))
+        let superclassIri = (IriReference "https://example.com/superclass")
+        let class2 = (ClassName (FullIri superclassIri))
+        let classIri3 = (IriReference "https://example.com/class3")
+        let class3 = (ClassName (FullIri classIri3))
+        let union = ObjectIntersectionOf [class1; class2; class3]
+        let translatedClass = Translator.translateClass logger union
+        translatedClass.Should().Be(Conjunction (ConceptName subclassIri, Conjunction (ConceptName superclassIri, ConceptName classIri3)))
+    
+    
+    [<Fact>]
+    let ``Complement of class name is translated`` () =
+        let classIri = (IriReference "https://example.com/class")
+        let class1 = (ClassName (FullIri classIri))
+        let complement = ObjectComplementOf class1
+        let translatedClass = Translator.translateClass logger complement
+        translatedClass.Should().Be(Negation (ConceptName classIri))
     
     [<Fact>]
     let ``Simple existential is translated`` () =
@@ -79,6 +149,28 @@ module Tests =
         let translatedClass = Translator.translateAssertion logger assertion
         translatedClass.Should().Be(ABoxAssertion.RoleAssertion (leftIri, rightIri, Role.Iri roleIri))
     
+    [<Fact>]
+    let ``Inverse object property fact is translated`` () =
+        let leftIri = (IriReference "https://example.com/left")
+        let rightIri = (IriReference "https://example.com/right")
+        let roleIri = (IriReference "https://example.com/role")
+        let role = InverseObjectProperty (NamedObjectProperty (FullIri roleIri))
+        let assertion = ObjectPropertyAssertion ([], role, NamedIndividual (FullIri leftIri),
+                                                 NamedIndividual (FullIri rightIri))
+        let translatedClass = Translator.translateAssertion logger assertion
+        translatedClass.Should().Be(ABoxAssertion.RoleAssertion (leftIri, rightIri, Role.Inverse roleIri))
+    
+    [<Fact>]
+    let ``Negative object property fact is translated`` () =
+        let leftIri = (IriReference "https://example.com/left")
+        let rightIri = (IriReference "https://example.com/right")
+        let roleIri = (IriReference "https://example.com/role")
+        let role = NamedObjectProperty (FullIri roleIri)
+        let assertion =  NegativeObjectPropertyAssertion ([], role, NamedIndividual (FullIri leftIri),
+                                                 NamedIndividual (FullIri rightIri))
+        let translatedClass = Translator.translateAssertion logger assertion
+        translatedClass.Should().Be(ABoxAssertion.NegativeAssertion (RoleAssertion (leftIri, rightIri, Role.Inverse roleIri)))
+    
     
     [<Fact>]
     let ``Simple data property fact is translated`` () =
@@ -92,72 +184,18 @@ module Tests =
         let translatedClass = Translator.translateAssertion logger assertion
         translatedClass.Should().Be(ABoxAssertion.LiteralAssertion (leftIri, roleIri, "(data)"))
     
+    
+    
     [<Fact>]
-    let ``Inverse object property fact is translated`` () =
+    let ``Negative data property fact is translated`` () =
         let leftIri = (IriReference "https://example.com/left")
-        let rightIri = (IriReference "https://example.com/right")
+        let data = GraphElement.GraphLiteral (RdfLiteral.LiteralString "data")
         let roleIri = (IriReference "https://example.com/role")
-        let role = InverseObjectProperty (NamedObjectProperty (FullIri roleIri))
-        let assertion = ObjectPropertyAssertion ([], role, NamedIndividual (FullIri leftIri),
-                                                 NamedIndividual (FullIri rightIri))
+        let role = FullIri roleIri
+        let assertion = NegativeDataPropertyAssertion ([], role,
+                                               NamedIndividual (FullIri leftIri),
+                                                data)
         let translatedClass = Translator.translateAssertion logger assertion
-        translatedClass.Should().Be(ABoxAssertion.RoleAssertion (leftIri, rightIri, Role.Inverse roleIri))
-    
-    [<Fact>]
-    let ``Simple class intersection is translated`` () =
-        let subclassIri = (IriReference "https://example.com/subclass")
-        let class1 = (ClassName (FullIri subclassIri))
-        let superclassIri = (IriReference "https://example.com/superclass")
-        let class2 = (ClassName (FullIri superclassIri))
-        let union = ObjectIntersectionOf [class1; class2]
-        let translatedClass = Translator.translateClass logger union
-        translatedClass.Should().Be(Conjunction (ConceptName subclassIri, ConceptName superclassIri))
+        translatedClass.Should().Be(ABoxAssertion.NegativeAssertion (ABoxAssertion.LiteralAssertion (leftIri, roleIri, "(data)")))
     
     
-    [<Fact>]
-    let ``Triple class union is translated`` () =
-        let subclassIri = (IriReference "https://example.com/subclass")
-        let class1 = (ClassName (FullIri subclassIri))
-        let superclassIri = (IriReference "https://example.com/superclass")
-        let class2 = (ClassName (FullIri superclassIri))
-        let classIri3 = (IriReference "https://example.com/class3")
-        let class3 = (ClassName (FullIri classIri3))
-        let union = ObjectUnionOf [class1; class2; class3]
-        let translatedClass = Translator.translateClass logger union
-        translatedClass.Should().Be(Disjunction (ConceptName subclassIri, Disjunction (ConceptName superclassIri, ConceptName classIri3)))
-    
-    
-    [<Fact>]
-    let ``Triple class intersection is translated`` () =
-        let subclassIri = (IriReference "https://example.com/subclass")
-        let class1 = (ClassName (FullIri subclassIri))
-        let superclassIri = (IriReference "https://example.com/superclass")
-        let class2 = (ClassName (FullIri superclassIri))
-        let classIri3 = (IriReference "https://example.com/class3")
-        let class3 = (ClassName (FullIri classIri3))
-        let union = ObjectIntersectionOf [class1; class2; class3]
-        let translatedClass = Translator.translateClass logger union
-        translatedClass.Should().Be(Conjunction (ConceptName subclassIri, Conjunction (ConceptName superclassIri, ConceptName classIri3)))
-    
-    [<Fact>]
-    let ``Single className is translated`` () =
-        let subclassIri = (IriReference "https://example.com/subclass")
-        let subClass = (ClassName (FullIri subclassIri))
-        let translatedClass = Translator.translateClass logger subClass
-        translatedClass.Should().Be(ConceptName subclassIri)
-    
-    [<Fact>]
-    let ``Single axiom Owl ontology is translated`` () =
-        let subclassIri = (IriReference "https://example.com/subclass")
-        let subClass = (ClassName (FullIri subclassIri))
-        let superclassIri = (IriReference "https://example.com/superclass")
-        let superClass = (ClassName (FullIri superclassIri))
-        let axiom = AxiomClassAxiom (SubClassOf ([],subClass, superClass)) 
-        let emptyOntology = new DagSemTools.OwlOntology.Ontology([],
-                                                                 DagSemTools.OwlOntology.ontologyVersion.UnNamedOntology,
-                                                                 [],
-                                                                 [axiom])
-        let translatedOntology = Translator.translate logger emptyOntology
-        let expectedAxiom = Inclusion (ConceptName subclassIri, ConceptName superclassIri) 
-        let expectedOntology = OntologyDocument.Ontology ([], ontologyVersion.UnNamedOntology, ([expectedAxiom],[]))
-        translatedOntology.Should().Be(expectedOntology)
