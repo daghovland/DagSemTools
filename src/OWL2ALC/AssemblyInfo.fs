38fac9af4f5bcb511644e0e18b41044347ad4c0b
Dag Hovland <hovlanddag@gmail.com>
2025-03-22 10:03:23 +0100

    typo in internal visible to

--- src/OWL2ALC/AssemblyInfo.fs
+++ src/OWL2ALC/AssemblyInfo.fs
@@ -3,6 +3,6 @@ module DagSemTools.OWL2ALC.AssemblyInfo
 open System.Runtime.CompilerServices
 
 [<assembly: InternalsVisibleTo("OWL2ALC.Tests")>]
-[<assembly: InternalsVisibleTo("Manchester2Alc.Tests")>]
+[<assembly: InternalsVisibleTo("DagSemTools.Manchester2Alc.Tests")>]
 [<assembly: InternalsVisibleTo("Api.Tests")>]
 do ()
