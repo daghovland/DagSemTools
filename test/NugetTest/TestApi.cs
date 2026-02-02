a0e014a4bddf95a89e97eb4de9894f974d3efbe6
Dag Hovland <hovlanddag@gmail.com>
2026-02-02 21:44:01 +0100

    Added lacking default graph fetch on nuget package tests

--- test/NugetTest/TestApi.cs
+++ test/NugetTest/TestApi.cs
@@ -15,7 +15,7 @@ public class TestApi(ITestOutputHelper output)
     public void Test1()
     {
         var ontology = new FileInfo("TestData/example1.ttl");
-        var ont = TriGParser.Parse(ontology, outputWriter);
+        var ont = TriGParser.Parse(ontology, outputWriter).GetDefaultGraph();
 
         Assert.NotNull(ont);
         var enemies = ont.GetTriplesWithPredicate(new IriReference("http://www.perceive.net/schemas/relationship/enemyOf"));
@@ -26,7 +26,7 @@ public class TestApi(ITestOutputHelper output)
     public void TestSparql()
     {
         var ontology = new FileInfo("TestData/example1.ttl");
-        var ont = TriGParser.Parse(ontology, outputWriter);
+        var ont = TriGParser.Parse(ontology, outputWriter).GetDefaultGraph();
 
         Assert.NotNull(ont);
         var enemies = ont.AnswerSelectQuery("SELECT * WHERE where{?hero <http://www.perceive.net/schemas/relationship/enemyOf> ?enemy.}").ToList();
@@ -47,7 +47,7 @@ public class TestApi(ITestOutputHelper output)
     public void TestDbPedia()
     {
         var ontology = new FileInfo("DbpediaTests/test2.ttl");
-        var ont = TriGParser.Parse(ontology, outputWriter);
+        var ont = TriGParser.Parse(ontology, outputWriter).GetDefaultGraph();
 
         Assert.NotNull(ont);
         var subjects = ont.GetDefaultGraph().GetTriplesWithPredicate(new IriReference("http://purl.org/dc/terms/subject"));
